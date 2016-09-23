91cdb8e1d7bb25e28a5edabc951b591f14c5f7be
Deepak Bora <deepakdefender264@gmail.com>
2016-09-23 10:29:24 +0000

    adding more initial roles

---
+++ ansiblemediawiki/MediaWiki.ps1
@@ -0,0 +1,29 @@
+# Tasks do we do ?
+# make a paramfile and keep it at the location where the script is running 
+webpicmd-x64.exe /install /products:MySQLWindows5.1 /application:"MediaWiki"@paramfile.app /AcceptEula /Language:en /MySQLPassword:123456aA /Log:log.txt
+#get the visual studio x64 redistributable 2k12 r2
+#wget https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe -outfile vcred-x86.exe
+#Start-Sleep -s 20
+# .\vcred.exe /passive
+ # done :)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
